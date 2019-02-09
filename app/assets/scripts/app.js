6edeae818ab184677a6459891bd43c731aa2640f
Josh <jvcoding9407@gmail.com>
2019-02-09 15:16:18 -0500

    Finished the static header function

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,10 @@
 import MobileMenu from "./modules/mobile_menu";
 import revealOnScroll from "./modules/revealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 import $ from "jquery";
 
 
 var mobileMenu = new MobileMenu();
 new revealOnScroll($(".feature-item"), "85%");
 new revealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
